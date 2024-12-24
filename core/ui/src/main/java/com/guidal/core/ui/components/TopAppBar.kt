eaa786159b02626c089196bb2fad7dc238a98096
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-12-24 22:16:57 +0200

    MDE/feature/Menu Adjusted top app bar default height

--- core/ui/src/main/java/com/guidal/core/ui/components/TopAppBar.kt
+++ core/ui/src/main/java/com/guidal/core/ui/components/TopAppBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -16,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.guidal.core.ui.models.UiModelTopAppBarIcon
 import com.guidal.core.ui.theme.GuidalIcons
 import com.guidal.core.ui.theme.GuidalTheme
@@ -51,8 +53,9 @@ fun TopAppBar(
             },
 
             // Optional
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(72.dp),
             navigationIcon = {
                 if (navigationIcon != null) {
                     Row(
