496cd458d5b11254e48dced6b6b2e89828727e6f
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-12-26 21:45:30 +0200

    MDE/feature/profile Removed unused import in GuidalIcons

--- core/ui/src/main/java/com/guidal/core/ui/theme/Icon.kt
+++ core/ui/src/main/java/com/guidal/core/ui/theme/Icon.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.guidal.core.ui.R
-import org.intellij.lang.annotations.Language
 
 /**
  * Module :core:ui
