394fd288b638e3b9196f856f5f88d88677c72040
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-12-26 21:44:55 +0200

    MDE/feature/profile Imported and defined new icons into GuidalIcons

--- core/ui/src/main/java/com/guidal/core/ui/theme/Icon.kt
+++ core/ui/src/main/java/com/guidal/core/ui/theme/Icon.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.guidal.core.ui.R
+import org.intellij.lang.annotations.Language
 
 /**
  * Module :core:ui
@@ -44,11 +45,31 @@ object GuidalIcons {
         val Settings: ImageVector,
         val Privacy: ImageVector,
         val About: ImageVector,
-        val Support: ImageVector
+        val Support: ImageVector,
+        val Id: ImageVector,
+        val Email: ImageVector,
+        val Password: ImageVector,
+        val Delete: ImageVector,
+        val Edit: ImageVector,
+        val Redirect: ImageVector
+    )
+
+    data class LanguageIcons(
+        val Other: ImageVector,
+        val Greece: ImageVector,
+        val Lithuania: ImageVector
+    )
+
+    data class GenderIcons(
+        val Other: ImageVector,
+        val Man: ImageVector,
+        val Woman: ImageVector
     )
 
     lateinit var Default: DefaultIcons
     lateinit var Outlined: OutlinedIcons
+    lateinit var Language: LanguageIcons
+    lateinit var Gender: GenderIcons
 
     @Composable
     internal fun Preload() {
@@ -73,7 +94,25 @@ object GuidalIcons {
             Settings = ImageVector.vectorResource(R.drawable.settings_outlined_icon),
             Privacy = ImageVector.vectorResource(R.drawable.privacy_outlined_icon),
             About = ImageVector.vectorResource(R.drawable.about_outlined_icon),
-            Support = ImageVector.vectorResource(R.drawable.support_outlined_icon)
+            Support = ImageVector.vectorResource(R.drawable.support_outlined_icon),
+            Id = ImageVector.vectorResource(R.drawable.id_outlined_icon),
+            Email = ImageVector.vectorResource(R.drawable.email_outlined_icon),
+            Password = ImageVector.vectorResource(R.drawable.password_outlined_icon),
+            Delete = ImageVector.vectorResource(R.drawable.delete_outlined_icon),
+            Edit = ImageVector.vectorResource(R.drawable.edit_outlined_icon),
+            Redirect = ImageVector.vectorResource(R.drawable.redirect_outlined_icon)
+        )
+
+        Language = LanguageIcons(
+            Other = ImageVector.vectorResource(R.drawable.language_other_icon),
+            Greece = ImageVector.vectorResource(R.drawable.language_greece_icon),
+            Lithuania = ImageVector.vectorResource(R.drawable.language_lithuania_icon)
+        )
+
+        Gender = GenderIcons(
+            Other = ImageVector.vectorResource(R.drawable.gender_other_icon),
+            Man = ImageVector.vectorResource(R.drawable.gender_man_icon),
+            Woman = ImageVector.vectorResource(R.drawable.gender_woman_icon)
         )
     }
 }
