7c0802e94ca7fff878f0d8380340c2b6d183e8b5
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2025-01-04 01:30:51 +0200

    MDE/feature/weather-widget :core:network module

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,82 @@
+import java.util.Properties
+
+// load the values from .properties file
+val keystoreFile = project.rootProject.file("local.properties")
+val properties = Properties()
+properties.load(keystoreFile.inputStream())
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.guidal.core.network"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField(
+                "String",
+                "OPENWEATHERMAP_API_KEY",
+                properties.getProperty("OPENWEATHERMAP_API_KEY")
+            )
+        }
+        debug {
+            buildConfigField(
+                "String",
+                "OPENWEATHERMAP_API_KEY",
+                properties.getProperty("OPENWEATHERMAP_API_KEY")
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_21.toString()
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    // Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    // Other
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
