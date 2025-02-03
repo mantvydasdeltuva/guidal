1d14a29f2f08f17759fd1896cfdb5a578d0a7dd1
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2025-02-03 10:39:48 +0200

    feature/floating-action-button Added FAB into locations list

--- feature/home/src/main/java/com/guidal/feature/home/location/LocationScreen.kt
+++ feature/home/src/main/java/com/guidal/feature/home/location/LocationScreen.kt
@@ -1,15 +1,24 @@
 package com.guidal.feature.home.location
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.imageResource
@@ -20,6 +29,7 @@ import com.guidal.core.ui.components.Scaffold
 import com.guidal.core.ui.components.TopAppBar
 import com.guidal.core.ui.models.UiModelTopAppBarIcon
 import com.guidal.core.ui.theme.GuidalIcons
+import kotlinx.coroutines.launch
 
 @Composable
 fun LocationScreen(
@@ -31,6 +41,7 @@ fun LocationScreen(
     val uiState by locationViewModel.uiState.collectAsState()
 
     val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
 
     // TODO: SWITCH TO FETCHING DATA FROM THE DATABASE
     // TODO: ADD TRANSLATIONS AFTER DATABASE FETCHING
@@ -58,6 +69,29 @@ fun LocationScreen(
                     size = dimensionResource(R.dimen.icon_size_16)
                 )
             )
+        },
+        floatingActionButton = {
+            AnimatedVisibility(
+                visible = scrollState.value.toFloat() >= 550f,
+                enter = fadeIn() + scaleIn(),
+                exit = fadeOut() + scaleOut()
+            ) {
+                FloatingActionButton(
+                    onClick = {
+                        coroutineScope.launch {
+                            scrollState.animateScrollTo(0)
+                        }
+                    },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onSurface,
+                ) {
+                    Icon(
+                        imageVector = GuidalIcons.Default.ChevronForward,
+                        contentDescription = "Scroll to Top",
+                        modifier = Modifier.rotate(-90f)
+                    )
+                }
+            }
         },
         modifier = modifier
     ) { innerPadding ->
